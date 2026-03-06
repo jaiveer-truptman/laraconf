e1cd173272606dc4ec4603bbab9f936e175460ba
jaiveer-truptman <jchavda@truptman.in>
2026-03-06 08:31:05 +0530

    wip : layout forms with section

--- app/Filament/Resources/Conferences/Schemas/ConferenceForm.php
+++ app/Filament/Resources/Conferences/Schemas/ConferenceForm.php
@@ -6,12 +6,12 @@ use App\Enums\Region;
 use App\Filament\Resources\Venues\Schemas\VenueForm;
 use Filament\Forms\Components\CheckboxList;
 use Filament\Forms\Components\DatePicker;
-use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\Select;
-use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class ConferenceForm
@@ -20,56 +20,79 @@ class ConferenceForm
     {
         return $schema
             ->components([
-                TextInput::make('name')
-                    ->label('Conference Name')
-                    ->helperText('Enter the name of the conference')
-                    ->hint('e.g., Laravel Live 2026')
-                    ->required(),
-                RichEditor::make('description')
-                    ->toolbarButtons([
-                        'bold',
-                        'italic',
-                        'underline',
-                        'link',
-                        'codeBlock',
-                    ])
-                    ->label('Conference Description')
-                    ->helperText('Provide a detailed description of the conference')
-                    ->required()
-                    ->maxLength(5000),
-                DatePicker::make('start_date')
-                    ->native(false)
-                    ->required(),
-                DatePicker::make('end_date')
-                    ->native(false)
-                    ->required(),
-                Select::make('status')
-                    ->options([
-                        0 => 'Draft',
-                        1 => 'Published',
-                        2 => 'Archived',
-                    ])
-                    ->required(),
-                Toggle::make('is_published')
-                    ->label('Published'),
-                Select::make('region')
-                    ->live()
-                    ->options(Region::class)
-                    ->enum(Region::class)
-                    ->required(),
-                Select::make('venue_id')
-                    ->required()
-                    ->preload()
-                    ->searchable()
-                    ->createOptionForm(fn (Schema $schema) => VenueForm::configure($schema))
-                    ->editOptionForm(fn (Schema $schema) => VenueForm::configure($schema))
-                    ->relationship('venue', 'name',modifyQueryUsing: function ($query, $get) {
-                        $query->where('region', $get('region'));
-                    }),
+
+                Section::make('Conference Details')
+                    ->collapsible()
+                    ->schema([
+                        TextInput::make('name')
+                            ->label('Conference Name')
+                            ->helperText('Enter the name of the conference')
+                            ->hint('e.g., Laravel Live 2026')
+                            ->required(),
+
+                        RichEditor::make('description')
+                            ->toolbarButtons([
+                                'bold',
+                                'italic',
+                                'underline',
+                                'link',
+                                'codeBlock',
+                            ])
+                            ->label('Conference Description')
+                            ->helperText('Provide a detailed description of the conference')
+                            ->required()
+                            ->maxLength(5000),
+
+                        Grid::make(2)
+                            ->schema([
+                                DatePicker::make('start_date')
+                                    ->native(false)
+                                    ->required()
+                                    ->columns(2),
+                                DatePicker::make('end_date')
+                                    ->native(false)
+                                    ->required()
+                                    ->columns(2),
+                            ]),
+                    ]),
+                    // ->columnSpanFull(),
+
+                Section::make('Location')
+                    ->schema([
+
+                        Select::make('region')
+                            ->live()
+                            ->options(Region::class)
+                            ->enum(Region::class)
+                            ->required(),
+                        Select::make('venue_id')
+                            ->required()
+                            ->preload()
+                            ->searchable()
+                            ->createOptionForm(fn (Schema $schema) => VenueForm::configure($schema))
+                            ->editOptionForm(fn (Schema $schema) => VenueForm::configure($schema))
+                            ->relationship('venue', 'name', modifyQueryUsing: function ($query, $get) {
+                                $query->where('region', $get('region'));
+                            }),
+                    ]),
+
+                Section::make('Visibility')
+                    ->description('Manage status and visibility of conference')
+                    ->schema([
+                        Select::make('status')
+                            ->options([
+                                0 => 'Draft',
+                                1 => 'Published',
+                                2 => 'Archived',
+                            ])
+                            ->required(),
+                        Toggle::make('is_published')
+                            ->label('Published'),
+                    ]),
 
                 CheckboxList::make('speakers')
                     ->relationship(titleAttribute: 'name')
-                    ->required()
+                    ->required(),
             ]);
     }
 }
