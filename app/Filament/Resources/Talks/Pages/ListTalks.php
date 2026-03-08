72b065a1790c2657a3ed4fa4c846421bcac233d6
jaiveer-truptman <jchavda@truptman.in>
2026-03-08 16:28:08 +0530

    filament table filters

--- app/Filament/Resources/Talks/Pages/ListTalks.php
+++ app/Filament/Resources/Talks/Pages/ListTalks.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Resources\Talks\Pages;
 
+use App\Enums\TalkStatus;
 use App\Filament\Resources\Talks\TalkResource;
+use App\Models\Talk;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListTalks extends ListRecords
 {
@@ -14,6 +18,36 @@ class ListTalks extends ListRecords
     {
         return [
             CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make()
+                ->badge(static fn (): int => Talk::count())
+                ->deferBadge(),
+            'submitted' => Tab::make()
+                ->badge(static fn (): int => Talk::whereStatus(TalkStatus::SUBMITTED)->count())
+                ->deferBadge()
+                ->badgeColor('warning')
+                ->modifyQueryUsing(function (Builder $query) {
+                    return $query->where('status', TalkStatus::SUBMITTED);
+                }),
+            'approved' => Tab::make()
+                ->badge(static fn (): int => Talk::whereStatus(TalkStatus::APPROVED)->count())
+                ->deferBadge()
+                ->badgeColor('success')
+                ->modifyQueryUsing(function (Builder $query) {
+                    return $query->where('status', TalkStatus::APPROVED);
+                }),
+            'rejected' => Tab::make()
+                ->badge(static fn (): int => Talk::whereStatus(TalkStatus::REJECTED)->count())
+                ->deferBadge()
+                ->badgeColor('danger')
+                ->modifyQueryUsing(function (Builder $query) {
+                    return $query->where('status', TalkStatus::REJECTED);
+                }),
         ];
     }
 }
--- app/Filament/Resources/Talks/Tables/TalksTable.php
+++ app/Filament/Resources/Talks/Tables/TalksTable.php
@@ -6,11 +6,14 @@ use App\Models\Talk;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
-use Filament\Tables\Columns\BadgeColumn;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Table;
 use Filament\Tables\Columns\ToggleColumn;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Filters\TernaryFilter;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
 
 class TalksTable
 {
@@ -23,8 +26,8 @@ class TalksTable
                     ->searchable(),
                 ImageColumn::make('speaker.avatar')
                     ->label('Speaker Avatar')
-                    ->defaultImageUrl(function(Talk $record) {
-                        return 'https://ui-avatars.com/api/?background=0D8ABC&color=fff/&name=' . urlencode($record->speaker->name);
+                    ->defaultImageUrl(function (Talk $record) {
+                        return 'https://ui-avatars.com/api/?background=0D8ABC&color=fff/&name='.urlencode($record->speaker->name);
                     })
                     ->circular(),
                 TextColumn::make('speaker.name')
@@ -36,12 +39,24 @@ class TalksTable
                     ->badge()
                     ->formatStateUsing(fn ($state) => $state->label())
                     ->sortable()
-                    ->color(function ($state){
+                    ->color(function ($state) {
                         return $state->getColor();
-                    })
+                    }),
             ])
             ->filters([
-                //
+                TernaryFilter::make('is_new'),
+                SelectFilter::make('speakers')
+                    ->relationship('speaker', 'name')
+                    ->multiple()
+                    ->searchable()
+                    ->preload(),
+                Filter::make('has_avatar')
+                    ->label('Speaker With Avatar')
+                    ->query(function (Builder $query) {
+                        $query->whereHas('speaker', function (Builder $query) {
+                            $query->whereNotNull('avatar');
+                        });
+                    }),
             ])
             ->recordActions([
                 EditAction::make(),
