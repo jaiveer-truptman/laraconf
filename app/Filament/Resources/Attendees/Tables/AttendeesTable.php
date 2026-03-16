990ebdeecd8b86535eb339ce2e638ece81e3266e
jaiveer-truptman <jchavda@truptman.in>
2026-03-16 19:32:36 +0530

    WIP with Attendees stats overview

---
+++ app/Filament/Resources/Attendees/Tables/AttendeesTable.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Filament\Resources\Attendees\Tables;
+
+use App\Models\Attendee;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class AttendeesTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable(),
+                TextColumn::make('email')
+                    ->label('Email address')
+                    ->searchable(),
+                TextColumn::make('ticket_cost')
+                    ->state(function(Attendee $record){
+                        return $record->ticket_cost / 100;
+                    })
+                    ->money()
+                    ->sortable(),
+                IconColumn::make('is_paid')
+                    ->boolean(),
+                TextColumn::make('conference_id')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
